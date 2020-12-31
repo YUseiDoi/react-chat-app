12272ca99622b721659b0c39219a3276c4852b75
土井悠生 <doiyuusei@doiyuuseinoMacBook-Pro.local>
2020-12-31 14:37:52 +0900

    2-20

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,15 @@ import React, {useState} from 'react';
 
 import SignIn from './SignIn.js';
 import Main from './Main';
+import config from '../config.json';
 
 export default () => {
   const [name, setName] = useState('');
   console.log(name);
 
-  if(name === ''){
+  if(config.signInEnabled && name === ''){
     return <SignIn setName={setName}/>;
   }else {
     return <Main name={name}/>;
   }
-  
 };
