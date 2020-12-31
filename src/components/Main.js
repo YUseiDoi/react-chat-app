a63072737bbaba423d266f97e24413eff44bf05e
土井悠生 <doiyuusei@doiyuuseinoMacBook-Pro.local>
2020-12-31 15:20:04 +0900

    2-22

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import MessageList from './MessageList';
+import MessageInputField from './MessageInputField';
 
-const Main = () => {
+const useStyles = makeStyles({
+    root: {
+        display: 'grid',
+        height: '100vh',
+        gridTemplateRows: '1fr auto',
+      },
+});
+
+const Main = ({name}) => {
+    const classes = useStyles();
     return (
-        <>
-        main
-        </>
-    )
+    <div className={classes.root}>
+        <MessageList />
+        <MessageInputField name={name}/>
+    </div>)
 }
 
 export default Main;
