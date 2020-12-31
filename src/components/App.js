ad7b271010b4e83af4c8ed649dbd51b71021b18a
土井悠生 <doiyuusei@doiyuuseinoMacBook-Pro.local>
2020-12-31 10:30:37 +0900

    2-17

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 import SignIn from './SignIn.js';
 
 export default () => {
-  const {name, setName} = useState('');
+  const [name, setName] = useState('');
   console.log(name);
   return <SignIn setName={setName}/>;
 };
